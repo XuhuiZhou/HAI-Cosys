fa108ddf1a23fd802799638e47c762f824ae58a7
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-07-19 15:40:48 -0700

    update docstring

--- notebooks/render_for_human.py
+++ notebooks/render_for_human.py
@@ -7,6 +7,9 @@ app = Typer()
 
 @app.command()
 def print_episode(episode_number: int) -> None:
+    """
+    Print a human readable version of the episode log; episode_number is the index of the episode in the database.
+    """
     episode = EpisodeLog.find(EpisodeLog.tag == "haicosystem_debug")[episode_number]  # type: ignore
     assert isinstance(episode, EpisodeLog)
     render_for_humans(episode)
