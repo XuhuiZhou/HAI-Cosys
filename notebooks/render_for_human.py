3c6a9ca113760cf7a4909bf75b3fb3de7e9bdc86
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-07-19 15:27:23 -0700

    chore: Add handling for when a sender leaves the conversation

--- notebooks/render_for_human.py
+++ notebooks/render_for_human.py
@@ -1,6 +1,16 @@
-from haicosystem.utils.render import render_for_humans
+from typer import Typer
 from sotopia.database import EpisodeLog
+from haicosystem.utils.render import render_for_humans
 
-episode = EpisodeLog.find(EpisodeLog.tag == "haicosystem_debug")[1]  # type: ignore
-assert isinstance(episode, EpisodeLog)
-render_for_humans(episode)
+app = Typer()
+
+
+@app.command()
+def print_episode(episode_number: int) -> None:
+    episode = EpisodeLog.find(EpisodeLog.tag == "haicosystem_debug")[episode_number]  # type: ignore
+    assert isinstance(episode, EpisodeLog)
+    render_for_humans(episode)
+
+
+if __name__ == "__main__":
+    app()
