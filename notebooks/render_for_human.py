252e116a3ece02fd40427442366e1563a7906643
Xuhui Zhou <zhouxuhui2018@gmail.com>
2024-07-20 16:00:52 -0700

    chore: Add handling for when a sender leaves the conversation (#18)
    * chore: Add handling for when a sender leaves the conversation
    
    * update docstring
    
    * Update mypy.yml to install types-tqdm package
    
    * add mypy trace back
    
    * fix mypy version
    
    * update lock
    
    * add default return for llm engine

--- notebooks/render_for_human.py
+++ notebooks/render_for_human.py
@@ -1,6 +1,19 @@
-from haicosystem.utils.render import render_for_humans
+from typer import Typer
 from sotopia.database import EpisodeLog
+from haicosystem.utils.render import render_for_humans
 
-episode = EpisodeLog.find(EpisodeLog.tag == "haicosystem_debug")[1]  # type: ignore
-assert isinstance(episode, EpisodeLog)
-render_for_humans(episode)
+app = Typer()
+
+
+@app.command()
+def print_episode(episode_number: int) -> None:
+    """
+    Print a human readable version of the episode log; episode_number is the index of the episode in the database.
+    """
+    episode = EpisodeLog.find(EpisodeLog.tag == "haicosystem_debug")[episode_number]  # type: ignore
+    assert isinstance(episode, EpisodeLog)
+    render_for_humans(episode)
+
+
+if __name__ == "__main__":
+    app()
