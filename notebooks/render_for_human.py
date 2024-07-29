c65fa6c0d042760e5896b869f44722e40c688346
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-07-28 18:12:28 -0700

    remove breakpoint

--- notebooks/render_for_human.py
+++ notebooks/render_for_human.py
@@ -14,7 +14,6 @@ def print_episode(episode_number: int) -> None:
     episode = EpisodeLog.find(EpisodeLog.tag == "haicosystem_debug")[episode_number]  # type: ignore
     assert isinstance(episode, EpisodeLog)
     messages = render_for_humans(episode)
-    breakpoint()
     rich_rendering(messages)
 
 
